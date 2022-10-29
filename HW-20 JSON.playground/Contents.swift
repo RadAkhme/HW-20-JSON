f2365a986654c126a267cc60caca8c102feae72e
RadikAkhme <a.radik.s@ya.ru>
2022-10-29 21:18:08 +0300

    Adding a function getData.

--- HW-20 JSON.playground/Contents.swift
+++ HW-20 JSON.playground/Contents.swift
@@ -10,3 +10,30 @@ func magicTheGatheringURL(_ cardName: String) -> String {
 
     return magicTheGatheringComponents.description
 }
+
+func getData(urlRequest: String) {
+    let urlRequest = URL(string: urlRequest)
+    guard let url = urlRequest else { return }
+    URLSession.shared.dataTask(with: url) { data, response, error in
+        if error != nil {
+            print("Error - \(String(describing: (error?.localizedDescription) ?? ""))")
+        }
+        
+        if let response = response as? HTTPURLResponse, response.statusCode == 200 {
+        }
+        
+        guard let data = data else { return }
+        let dataAsString = String(data: data, encoding: .utf8)
+        
+        let decoder = JSONDecoder()
+        do {
+            let mtg = try decoder.decode(Cards.self, from: data)
+            print(mtg.printAllCards())
+        } catch {
+            print(error)
+        }
+    }.resume()
+}
+
+getData(urlRequest: magicTheGatheringURL("Black Lotus"))
+getData(urlRequest: magicTheGatheringURL("Opt"))
