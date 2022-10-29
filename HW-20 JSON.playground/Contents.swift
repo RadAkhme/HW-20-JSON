d6616b4f84014fad68089bfe660f6c894ecd2601
RadikAkhme <a.radik.s@ya.ru>
2022-10-29 21:17:07 +0300

    Adding a function to create URL.

--- HW-20 JSON.playground/Contents.swift
+++ HW-20 JSON.playground/Contents.swift
@@ -1,1 +1,12 @@
 import UIKit
+
+
+func magicTheGatheringURL(_ cardName: String) -> String {
+    var magicTheGatheringComponents = URLComponents()
+    magicTheGatheringComponents.scheme = "https"
+    magicTheGatheringComponents.host = "api.magicthegathering.io"
+    magicTheGatheringComponents.path = "/v1/cards"
+    magicTheGatheringComponents.queryItems = [URLQueryItem(name: "name", value: cardName)]
+
+    return magicTheGatheringComponents.description
+}
