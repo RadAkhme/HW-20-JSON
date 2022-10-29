742636287d2516e40c1b0f0f0742b824026adb5f
RadikAkhme <a.radik.s@ya.ru>
2022-10-29 21:16:21 +0300

    Create Model.

---
+++ HW-20 JSON.playground/Sources/Model.swift
@@ -0,0 +1,30 @@
+import Foundation
+
+
+public struct Cards: Decodable {
+    var cards: [Card]
+    
+    public func printAllCards() {
+        for card in cards {
+            card.printCard()
+        }
+    }
+}
+
+struct Card: Decodable {
+    var name: String
+    var manaCost: String?
+    var type: String
+    var setName: String
+    var text: String
+    var artist: String
+    
+    func printCard() {
+        print("\nНазвание карты: \(name)")
+        print("Мановая Стоимость: \(manaCost ?? "У данной карты не было мановой стоимости")")
+        print("Тип: \(type)")
+        print("Текст: \(text)")
+        print("Название сета: \(setName)")
+        print("Автор: \(artist)")
+    }
+}
