360115f751ff2d62f34207e3824c24fbbd672748
RadikAkhme <a.radik.s@ya.ru>
2022-10-30 23:06:38 +0300

    Fix code in func getData.

--- HW-20 JSON.playground/Contents.swift
+++ HW-20 JSON.playground/Contents.swift
@@ -1,39 +1,36 @@
 import Foundation
 
 
-func magicTheGatheringURL(_ cardName: String) -> String {
-    var magicTheGatheringComponents = URLComponents()
-    magicTheGatheringComponents.scheme = "https"
-    magicTheGatheringComponents.host = "api.magicthegathering.io"
-    magicTheGatheringComponents.path = "/v1/cards"
-    magicTheGatheringComponents.queryItems = [URLQueryItem(name: "name", value: cardName)]
-
-    return magicTheGatheringComponents.description
+func getData(cardsUrlRequest: [String]) {
+    for request in cardsUrlRequest {
+        var magicTheGatheringComponents = URLComponents()
+        magicTheGatheringComponents.scheme = "https"
+        magicTheGatheringComponents.host = "api.magicthegathering.io"
+        magicTheGatheringComponents.path = "/v1/cards"
+        magicTheGatheringComponents.queryItems = [URLQueryItem(name: "name", value: request)]
+        
+        let urlRequest = URL(string: magicTheGatheringComponents.description)
+        guard let url = urlRequest else { return }
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            if error != nil {
+                print("Error - \(String(describing: (error?.localizedDescription) ?? ""))")
+            }
+            
+            guard let response = response as? HTTPURLResponse, response.statusCode == 200 else { return }
+            
+            guard let data = data else { return }
+            let dataAsString = String(data: data, encoding: .utf8)
+            
+            let decoder = JSONDecoder()
+            do {
+                let mtg = try decoder.decode(Cards.self, from: data)
+                print(mtg.printAllCards(cardsUrlRequest))
+            } catch {
+                print(error)
+            }
+        }.resume()
+    }
 }
 
-func getData(urlRequest: String) {
-    let urlRequest = URL(string: urlRequest)
-    guard let url = urlRequest else { return }
-    URLSession.shared.dataTask(with: url) { data, response, error in
-        if error != nil {
-            print("Error - \(String(describing: (error?.localizedDescription) ?? ""))")
-        }
-        
-        if let response = response as? HTTPURLResponse, response.statusCode == 200 {
-        }
-        
-        guard let data = data else { return }
-        let dataAsString = String(data: data, encoding: .utf8)
-        
-        let decoder = JSONDecoder()
-        do {
-            let mtg = try decoder.decode(Cards.self, from: data)
-            print(mtg.printAllCards())
-        } catch {
-            print(error)
-        }
-    }.resume()
-}
 
-getData(urlRequest: magicTheGatheringURL("Black Lotus"))
-getData(urlRequest: magicTheGatheringURL("Opt"))
+getData(cardsUrlRequest: ["Black Lotus", "Opt"])
